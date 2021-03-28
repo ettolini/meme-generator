82ebcfb1cddd31b605928f52cb38a93bc7e63b0f
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-28 18:22:55 -0300

    Add meme img

--- components/MemeItem.tsx
+++ components/MemeItem.tsx
@@ -5,6 +5,7 @@ const MemeItem = (props: any) => {
         <Link href="/meme/[id]" as={`/meme/${props.meme.id}`}>
             <a>
                 <h3>{props.meme.name}</h3>
+                <img src={props.meme.url}/>
             </a>
         </Link>
     )
